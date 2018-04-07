2b04a41bf2aba33cfb7e2c086221e5cbfe34538d
Josephaberry54 <josephaberry54@gmail.com>
2018-04-07 14:55:08 +0100

    styling search box

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,11 +4,21 @@ import { Link } from "react-router-dom";
 const Search = () => {
   return (
     <div>
-      <div className="row">
-        <input className="col" type="text" />
-        <button className="col btn btn-light">Search</button>
-      </div>
-      <Link to="/search">search all topics</Link>
+      <form className="row">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="search articles"
+          />
+          <div className="input-group-append">
+            <button className="btn btn-outline-secondary" type="button">
+              <i className="fas fa-search" />
+            </button>
+          </div>
+        </div>
+      </form>
+      <Link to="/search">view all topics</Link>
     </div>
   );
 };
